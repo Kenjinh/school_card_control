17fea5d222db763ca76bf5ddcd2ad71b5396dc17
gabriel.kenji <gabriel.kenji@ongame.com.br>
2023-07-11 12:58:45 -0300

    feat(backend/frontend): Add docker

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/4.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure-p$&%70fw-bg3z-1@x1s2omlt!(1^z_c6!qy&njj)0o92yt5p*w"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = (os.environ.get('DEBUG'), False)
 
 ALLOWED_HOSTS = []
 
@@ -75,8 +75,13 @@ WSGI_APPLICATION = "backend.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": "django.db.backends.mysql",
+        "NAME": os.environ.get('DB_NAME'),
+        "USER": os.environ.get('DB_USER'),
+        "PASSWORD": os.environ.get('DB_PASSWORD'),
+        "HOST": os.environ.get('DB_HOST'),
+        "PORT": os.environ.get('DB_PORT'),
+        "OPTIONS": {'charset': 'utf8mb4'}
     }
 }
 
