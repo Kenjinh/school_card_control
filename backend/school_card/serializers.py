3790237749d800e6b66b590bff58c0f3f1646068
gabriel.kenji <gabriel.kenji@ongame.com.br>
2023-07-13 12:05:26 -0300

    feat(backend): Created School Card API

---
+++ backend/school_card/serializers.py
@@ -0,0 +1,40 @@
+from rest_framework import serializers
+
+from .models import Grade, SchoolCard
+
+
+
+class GradeSerializer(serializers.ModelSerializer):
+    subject_name = serializers.StringRelatedField(source='subject', read_only=True)
+
+    def create(self, validated_data):
+        return Grade.objects.create(**validated_data)
+
+    def update(self, instance, validated_data):
+        instance.school_card = validated_data.get('school_card', instance.student)
+        instance.subject = validated_data.get('subject', instance.delivery_date)
+        instance.grade = validated_data.get('grade', instance.student)
+        instance.save()
+        return instance
+
+    class Meta:
+        model = Grade
+        fields = '__all__'
+
+
+class SchoolCardSerializer(serializers.ModelSerializer):
+    student_name = serializers.StringRelatedField(source='student', read_only=True)
+    grades = GradeSerializer(source='grade_set', read_only=True, many=True)
+
+    def create(self, validated_data):
+        return SchoolCard.objects.create(**validated_data)
+
+    def update(self, instance, validated_data):
+        instance.delivery_date = validated_data.get('delivery_date', instance.delivery_date)
+        instance.student = validated_data.get('student', instance.student)
+        instance.save()
+        return instance
+
+    class Meta:
+        model = SchoolCard
+        fields = '__all__'
---
+++ backend/school_card/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+
+from .views import GradeListAPI, GradeDetailAPI, SchoolCardListAPI, SchoolCardDetailAPI
+
+urlpatterns = [
+    path('list/', SchoolCardListAPI.as_view(),                                                 name="school_card_list"),
+    path('list/<int:pk>', SchoolCardDetailAPI.as_view(),                                     name="school_card_detail"),
+    path('grade/list/', GradeListAPI.as_view(),                                                      name="grade_list"),
+    path('grade/list/<int:pk>', GradeDetailAPI.as_view(),                                          name="grade_detail"),
+]
